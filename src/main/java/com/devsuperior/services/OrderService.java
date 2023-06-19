20eb54c93e62a09cdc7016e59c3e588371605438
Diogo Lima <filosofo_carpediem@hotmail.com>
2023-06-19 12:05:44 -0300

    Components created

---
+++ src/main/java/com/devsuperior/services/OrderService.java
@@ -0,0 +1,12 @@
+package com.devsuperior.services;
+
+import com.devsuperior.entities.Order;
+
+public class OrderService {
+
+    private ShippingService shippingService;
+
+    public double total(Order order){
+        return 0.0;
+    }
+}
---
+++ src/main/java/com/devsuperior/services/ShippingService.java
@@ -0,0 +1,10 @@
+package com.devsuperior.services;
+
+import com.devsuperior.entities.Order;
+
+public class ShippingService {
+
+    public double shipment(Order order){
+        return 0.0;
+    }
+}

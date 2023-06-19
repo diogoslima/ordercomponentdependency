a731327a23aab0d5569c1ba4d6faf5dac1bbbdbd
Diogo Lima <filosofo_carpediem@hotmail.com>
2023-06-19 14:08:27 -0300

    Order service logic refactored

--- src/main/java/com/devsuperior/services/OrderService.java
+++ src/main/java/com/devsuperior/services/OrderService.java
@@ -10,6 +10,6 @@ public class OrderService {
     private ShippingService shippingService;
 
     public double total(Order order){
-        return order.getBasic() + (order.getBasic() * order.getDiscount()) + shippingService.shipment(order);
+        return order.getBasic() - (order.getBasic() * order.getDiscount()/100) + shippingService.shipment(order);
     }
 }

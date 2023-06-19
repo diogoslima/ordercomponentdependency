b69fbdd02aaadac05d955be85d0f282a7478c9cf
Diogo Lima <filosofo_carpediem@hotmail.com>
2023-06-19 11:17:58 -0300

    Maven Project Created

---
+++ src/main/java/com/devsuperior/orderComponentsDependencyChallenge/OrderComponentsDependencyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.orderComponentsDependencyChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderComponentsDependencyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderComponentsDependencyChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/orderComponentsDependencyChallenge/OrderComponentsDependencyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.orderComponentsDependencyChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderComponentsDependencyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

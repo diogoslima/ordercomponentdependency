0e9f061bf15df823d86bc945af56616be04f40c8
Diogo Lima <filosofo_carpediem@hotmail.com>
2023-06-19 11:47:11 -0300

    Implemented CommandLineRunner

--- src/main/java/com/devsuperior/orderComponentsDependencyChallenge/OrderComponentsDependencyChallengeApplication.java
+++ src/main/java/com/devsuperior/orderComponentsDependencyChallenge/OrderComponentsDependencyChallengeApplication.java
@@ -1,13 +1,18 @@
 package com.devsuperior.orderComponentsDependencyChallenge;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderComponentsDependencyChallengeApplication {
+public class OrderComponentsDependencyChallengeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderComponentsDependencyChallengeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+	}
 }
